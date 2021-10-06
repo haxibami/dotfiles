1ec5af14129d2fde8f540990063d5494f8bc9513
haxibami <haxibami@gmail.com>
2021-10-06 11:38:03 +0900

    october first commit

---
+++ .bin/lineapp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/opt/google/chrome/google-chrome --profile-directory=Default --app-id=pbpoojghcbigbheeamegjbdailkaeglf
--- .bin/swaypulse.sh
+++ .bin/swaypulse.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# a script to choose pulseaudio default sink and source, via wofi, pamixer, pactl.
-# requires: bash, wofi, pamixer, pactl (you can also use dmenu alternatives: eg. rofi, dmenu)
+# a script to choose pulseaudio default sink and source via wofi/pamixer/pactl, on wlroots-based compositor session.
+# requirement: bash, wofi, pamixer, pactl (you may also use other wofi alternatives: eg. rofi, dmenu)
 
 #sinklist=$(pamixer --list-sinks | grep output | awk -F\" '{print $2}' | tr ' ' '\n')
 #sinknamelist=$(pamixer --list-sinks | grep output | awk -F\" '{print $4}')
 
 setdefault () {
-  menu=$(pamixer --list-sources | grep $2 | awk -F\" '{print $2}' | wofi -i -b -p $1-list --dmenu --width 1000 --lines=5 --style=${HOME}/.config/wofi/powermenu.css --conf=${HOME}/.config/wofi/powermenu.conf) &&
+  menu=$(pamixer --list-$1s | grep -e $2 -e $1 | awk -F\" '{print $2}' | wofi -i -b -p $1-list --dmenu --width 1000 --lines=5 --style=${HOME}/.config/wofi/powermenu.css --conf=${HOME}/.config/wofi/powermenu.conf) &&
     pactl set-default-$1 ${menu}
 }
 
@@ -21,15 +21,3 @@ pref=$(echo -e " input\n墳 output" | wofi -i -b -p in/out --dmenu --width 30
       ;;
 
   esac
-
-
-#menu=$(pamixer --list-${target} | grep ${io} | awk -F\" '{print $2}' | wofi -i -b -p sink-list --dmenu --width 1000 --lines=5 --style=${HOME}/.config/wofi/powermenu.css --conf=${HOME}/.config/wofi/powermenu.conf) &&
-
-#for sink in $menu
-#sink=$(ponymix -t sink list|awk '/^sink/ {s=$1" "$2;getline;gsub(/^ +/,"",$0);print s" "$0}'| wofi --dmenu -p 'pulseaudio sink:' --width 500 | grep -Po '[0-9]+(?=:)') &&
-
-#ponymix set-default -d $sink &&
-#for input in $(ponymix list -t sink-input|grep -Po '[0-9]+(?=:)');do
-#	echo "$input -> $sink"
-#	ponymix -t sink-input -d $input move $sink
-#done
